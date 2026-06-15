a8bed0168221778a0333d825f9ec8fb3ac7e18af
Alwyal <alwyal2001@gmail.com>
2026-06-16 00:18:42 +0700

    update game1

--- js/game1.js
+++ js/game1.js
@@ -1,7 +1,7 @@
 window.startLevel1 = function() {
     let score = 0;
     let missed = 0;
-    const targetScore = 66; 
+    const targetScore = 50; 
     const maxMissed = 6;
     let gameInterval;
     let isGameOver = false;
